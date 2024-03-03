a5c05dfe482619bc8965d7f4f85b0d77a7d08d7d
Vytautas <bunvytt@gmail.com>
2024-03-03 12:18:51 +0200

    Added fullscreen button

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+if (document.getElementById('fullscreenBtn')) {
+  document.getElementById('fullscreenBtn').addEventListener('click', function() {
+    var elem = document.getElementById('myIframe');
+    if (!elem.classList.contains('fullscreen')) {
+      elem.classList.add('fullscreen');
+      this.innerHTML = 'Exit Fullscreen';
+    } else {
+      elem.classList.remove('fullscreen');
+      this.innerHTML = 'Go Fullscreen';
+    }
+  });
+}
